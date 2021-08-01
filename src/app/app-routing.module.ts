d2850dc40d4280000a76c7677b872bf6d88ebd6f
Luis Eduardo Alcantara Olvera <shinobi_luis@hotmail.es>
2021-07-31 19:04:53 -0500

    Se creo el home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// componentes de pagina
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '**',  pathMatch: 'full',  redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
